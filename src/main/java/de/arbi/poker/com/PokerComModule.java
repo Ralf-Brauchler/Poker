10c90c15c9d03e97ffa1c73bf02c3bfcde8b9315
codestoe <codestoe@codestoe.de>
2016-09-18 16:57:20 +0200

    added binding of JoinGameHandler

--- src/main/java/de/arbi/poker/com/PokerComModule.java
+++ src/main/java/de/arbi/poker/com/PokerComModule.java
@@ -3,13 +3,13 @@ package de.arbi.poker.com;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 import de.arbi.poker.PokerHandler;
-import de.arbi.poker.PokerService;
-import de.arbi.poker.PokerServiceImpl;
+import de.arbi.poker.handlers.JoinGameHandler;
 import ratpack.handling.HandlerDecorator;
 
 public class PokerComModule extends AbstractModule {
     protected void configure() {
         bind(PokerHandler.class);
+        bind(JoinGameHandler.class);
         Multibinder.newSetBinder(binder(), HandlerDecorator.class).addBinding().toInstance(HandlerDecorator.prepend(new de.arbi.poker.LoggingHandler()));
     }
 }
